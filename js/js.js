295505c553fa2c430eceea54997801339ce88b41
ippotech <73618131+ippotech@users.noreply.github.com>
2021-07-03 12:54:46 -0500

    very basic nav menu

--- js/js.js
+++ js/js.js
@@ -1,6 +1,8 @@
 const $headerMenu = document.querySelector('.icon-menu')
 const $ul = document.querySelector('#header ul')
 
+
+
 const activateMenu = () => {
     $ul.style.display === 'none' ? $ul.style.display = 'flex' : $ul.style.display = 'none'
 }
