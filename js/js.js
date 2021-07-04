79f9db34adebf8b1a28e669b81e1eaccb65152cd
ippotech <73618131+ippotech@users.noreply.github.com>
2021-07-04 00:06:20 -0500

    nav ready

--- js/js.js
+++ js/js.js
@@ -1,10 +1,11 @@
 const $headerMenu = document.querySelector('.icon-menu')
 const $ul = document.querySelector('#header ul')
 
-
+console.log($ul.style.animationIterationCount = 1)
 
 const activateMenu = () => {
     $ul.style.display === 'none' ? $ul.style.display = 'flex' : $ul.style.display = 'none'
 }
 
 $headerMenu.addEventListener('click',activateMenu)
+
