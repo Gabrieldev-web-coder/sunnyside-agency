49872ab29d7cce4231efd6b3b133388a67306879
ippotech <gabrielgaymer04@gmail.com>
2021-07-05 22:30:36 -0500

    tryOf

--- js/js.js
+++ js/js.js
@@ -1,11 +1,12 @@
 const $headerMenu = document.querySelector('.icon-menu')
 const $ul = document.querySelector('#header ul')
 
-console.log($ul.style.animationIterationCount = 1)
-
 const activateMenu = () => {
-    $ul.style.display === 'none' ? $ul.style.display = 'flex' : $ul.style.display = 'none'
-}
+    $ul.classList.contains('animation') ? $ul.classList.add('animation-backwards') : $ul.classList.add('animation')
+    if($ul.classList.contains('animation') && $ul.classList.contains('animation-backwards')){
+        $ul.classList.remove('animation')
+        $ul.classList.remove('animation-backwards')
+    }
+} 
 
 $headerMenu.addEventListener('click',activateMenu)
-
