41461c56c22fc6beb669b4aa202328225892b8da
ippotech <gabrielgaymer04@gmail.com>
2021-07-07 15:10:40 -0500

    ready hamburguer menu

--- js/js.js
+++ js/js.js
@@ -1,12 +1,9 @@
 const $headerMenu = document.querySelector('.icon-menu')
 const $ul = document.querySelector('#header ul')
 
+
 const activateMenu = () => {
-    $ul.classList.contains('animation') ? $ul.classList.add('animation-backwards') : $ul.classList.add('animation')
-    if($ul.classList.contains('animation') && $ul.classList.contains('animation-backwards')){
-        $ul.classList.remove('animation')
-        $ul.classList.remove('animation-backwards')
-    }
-} 
+    $ul.classList.toggle('animation')
+}
 
 $headerMenu.addEventListener('click',activateMenu)
